bfede8375f84c5f4e1f05ec89d09033babcd1115
lhs-devloper <sdsd0114@naver.com>
2022-08-03 17:29:29 +0900

    2022_08_03 leave work commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 본 Repository에서는 Google Cloud Vision APi에 대해 작성하였습니다.
+## Cloud Vision API Doucmentation
+### Link: https://cloud.google.com/vision/docs/quickstarts
+___
+## 기본 요구사항
+1. RESTFUL API지식(JSON형식으로 Request, Response를 할 수 있어야합니다.)
+2. Google Cloud Vision설정에 대한 지식이 있어야합니다.
+___
---
+++ javscript_kgsearch/JS_kgSearch.js
@@ -0,0 +1,18 @@
+const baseURL = 'https://kgsearch.googleapis.com/v1/entities:search'; 
+const API_KEY = `${YOUR_API_KEY}`; // Google Cloud Vision API_KEY
+const m_id = '/m/03c_qk' // Machine Learning ID
+const params = {
+    'ids': m_id,
+    'limit': 10,
+    'indent': true,
+    'languages': 'ko',
+    'key': API_KEY,
+};
+const SearchParams = new URLSearchParams(params).toString();
+const final_url = `${baseURL}?${SearchParams}`;
+// console.log(final_url);
+const m_id_inf = await fetch(final_url, {
+    method: "GET"
+})
+const reponse = m_id_inf.json();
+// console.log(reponse);
